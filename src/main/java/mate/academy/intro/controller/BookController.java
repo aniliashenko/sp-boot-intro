a314c4b808d6c2ecc26c89f1181f85124f82f2e0
AntonIliashenko <windsweptofficial@gmail.com>
2025-07-22 18:40:45 +0200

    third-hw

--- src/main/java/mate/academy/intro/controller/BookController.java
+++ src/main/java/mate/academy/intro/controller/BookController.java
@@ -5,7 +5,15 @@ import lombok.RequiredArgsConstructor;
 import mate.academy.intro.dto.BookDto;
 import mate.academy.intro.dto.CreateBookRequestDto;
 import mate.academy.intro.service.BookService;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RestController
@@ -31,7 +39,8 @@ public class BookController {
 
     @PutMapping("/{id}")
     @ResponseBody
-    public BookDto updateBookById(@PathVariable Long id, @RequestBody CreateBookRequestDto requestDto) {
+    public BookDto updateBookById(@PathVariable Long id,
+                                  @RequestBody CreateBookRequestDto requestDto) {
         return bookService.updateBookById(id, requestDto);
     }
 
