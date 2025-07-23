00b2bd83986d7c41422e4798b7cb5bd894b07fed
AntonIliashenko <windsweptofficial@gmail.com>
2025-07-23 18:28:35 +0200

    5-hw-fix-2

--- src/main/java/mate/academy/intro/dto/CreateBookRequestDto.java
+++ src/main/java/mate/academy/intro/dto/CreateBookRequestDto.java
@@ -10,9 +10,9 @@ import lombok.Data;
 public class CreateBookRequestDto {
     @NotBlank
     private String title;
-    @NotNull
+    @NotBlank
     private String author;
-    @NotNull
+    @NotBlank
     private String isbn;
     @NotNull
     @Positive
