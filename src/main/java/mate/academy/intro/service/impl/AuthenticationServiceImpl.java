18c686886ddec5eccac37df2bc6d6d4f3c02dcf4
AntonIliashenko <windsweptofficial@gmail.com>
2025-08-01 13:49:17 +0200

    7-hw-security-fix-2

--- src/main/java/mate/academy/intro/service/impl/AuthenticationServiceImpl.java
+++ src/main/java/mate/academy/intro/service/impl/AuthenticationServiceImpl.java
@@ -20,7 +20,7 @@ public class AuthenticationServiceImpl implements AuthenticationService {
     @Override
     public UserResponseDto register(UserRegistrationRequestDto requestDto)
             throws RegistrationException {
-        Optional<User> existingUser = userRepository.findByEmail(requestDto.getEmail());
+        Optional<User> existingUser = userRepository.existByEmail(requestDto.getEmail());
         if (existingUser.isPresent()) {
             throw new RegistrationException("User with this email already exists");
         }
