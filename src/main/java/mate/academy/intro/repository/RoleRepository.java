080c6b58ec1284db02c61e311a6b26e0fcf0a43a
AntonIliashenko <windsweptofficial@gmail.com>
2025-08-03 09:30:50 +0200

    8-hw-security-fix-2

--- src/main/java/mate/academy/intro/repository/RoleRepository.java
+++ src/main/java/mate/academy/intro/repository/RoleRepository.java
@@ -3,10 +3,7 @@ package mate.academy.intro.repository;
 import java.util.Optional;
 import mate.academy.intro.model.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 public interface RoleRepository extends JpaRepository<Role, Long> {
-    //@Query(nativeQuery = true, value = "SELECT r FROM roles AS r WHERE r.roleName LIKE :roleName")
-    Optional<Role> findByRoleName(@Param("roleName") Role.RoleName roleName);
+    Optional<Role> findByRole(Role.RoleName role);
 }
