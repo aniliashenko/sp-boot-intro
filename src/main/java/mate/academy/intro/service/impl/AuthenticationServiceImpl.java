8f9ab85ca04ec4b14ef174cc9b06d4a5fdc849a8
AntonIliashenko <windsweptofficial@gmail.com>
2025-08-03 09:13:28 +0200

    7-hw-security-fix-3

--- src/main/java/mate/academy/intro/service/impl/AuthenticationServiceImpl.java
+++ src/main/java/mate/academy/intro/service/impl/AuthenticationServiceImpl.java
@@ -20,7 +20,8 @@ public class AuthenticationServiceImpl implements AuthenticationService {
     public UserResponseDto register(UserRegistrationRequestDto requestDto)
             throws RegistrationException {
         if (userRepository.existsByEmail(requestDto.getEmail())) {
-            throw new RegistrationException("User with this email already exists: " + requestDto.getEmail());
+            throw new RegistrationException(
+                    "User with this email already exists: " + requestDto.getEmail());
         }
 
         User user = userMapper.toModel(requestDto);
