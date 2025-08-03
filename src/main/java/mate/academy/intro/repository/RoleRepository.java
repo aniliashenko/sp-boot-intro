7c56c482238d35ad0179f076b131acf883c903b6
AntonIliashenko <windsweptofficial@gmail.com>
2025-08-03 09:19:50 +0200

    8-hw-security

--- src/main/java/mate/academy/intro/repository/RoleRepository.java
+++ src/main/java/mate/academy/intro/repository/RoleRepository.java
@@ -7,6 +7,6 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface RoleRepository extends JpaRepository<Role, Long> {
-    @Query(nativeQuery = true, value = "SELECT r FROM roles AS r WHERE r.roleName LIKE :roleName")
+    //@Query(nativeQuery = true, value = "SELECT r FROM roles AS r WHERE r.roleName LIKE :roleName")
     Optional<Role> findByRoleName(@Param("roleName") Role.RoleName roleName);
 }
--- src/main/java/mate/academy/intro/service/impl/AuthenticationServiceImpl.java
+++ src/main/java/mate/academy/intro/service/impl/AuthenticationServiceImpl.java
@@ -1,6 +1,5 @@
 package mate.academy.intro.service.impl;
 
-import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import mate.academy.intro.dto.UserRegistrationRequestDto;
 import mate.academy.intro.dto.UserResponseDto;
@@ -20,8 +19,7 @@ public class AuthenticationServiceImpl implements AuthenticationService {
     @Override
     public UserResponseDto register(UserRegistrationRequestDto requestDto)
             throws RegistrationException {
-        Optional<User> existingUser = userRepository.findByEmail(requestDto.getEmail());
-        if (existingUser.isPresent()) {
+        if (userRepository.existsByEmail(requestDto.getEmail())) {
             throw new RegistrationException("User with this email already exists");
         }
 
